7aae8c13ba5628dc2bb62cb9506de9c6563cc58d
Xin Lu <luxinexcs@gmail.com>
2015-11-09 16:57:39 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+
+
+var middleware = require('./middleware.js');
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req, res) {
+	res.send('About us');
+})
+app.use(express.static(__dirname + '/public'));
+app.listen(3000, function() {
+	console.log('Express server start');
+});
